1425667fdd1d42397c8691251d445dad8798dfcd
EYBhargav <eybhargav@gmail.com>
2023-08-10 15:37:15 +0530

    first commit

---
+++ libs/insta-pasa-redux-store/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/insta-pasa-redux-store';
---
+++ libs/insta-pasa-redux-store/src/lib/insta-pasa-redux-store.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface InstaPasaReduxStoreProps {}
+
+export function InstaPasaReduxStore(props: InstaPasaReduxStoreProps) {
+  return (
+    <div>
+      <h1>Welcome to InstaPasaReduxStore!</h1>
+    </div>
+  );
+}
+
+export default InstaPasaReduxStore;
